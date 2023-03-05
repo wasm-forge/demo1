60d27a9fbc14d8f493a21f6d0745e67439906fd6
Stanislav Golubtsov <sgaflv@gmail.com>
2023-03-05 17:26:42 +0100

    add polyfill dependency

--- src/demo1_backend/src/lib.rs
+++ src/demo1_backend/src/lib.rs
@@ -1,4 +1,12 @@
 #[ic_cdk_macros::query]
 fn greet(name: String) -> String {
+    
+    ic_cdk::api::print(format!("Hello from IC debugger: {}", name));
+    println!("Hello from WASI: {}", name);
+
     format!("Hello, {}!", name)
+}
+
+fn init() {
+    ic_polyfill::init();
 }
